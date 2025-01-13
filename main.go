65e9ba0cf6a616cf0f0c6d5cafba83183f106e24
yash-raj10 <yashraj.se10@gmail.com>
2025-01-14 01:46:11 +0530

    gn

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
 
@@ -23,7 +30,7 @@ func datastore(c *gin.Context) {
 		c.JSON(400, gin.H{"Message ": "Collection or DB name is missing"})
 	}
 
-	var inputData map[string]interface{}
+	var inputData []map[string]interface{}
 
 	if err := c.ShouldBind(&inputData); err != nil {
 		c.JSON(400, gin.H{"error": "send good jsonm you fucker"})
@@ -36,7 +43,29 @@ func datastore(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "saved data"})
 }
 
-func saveData(DBname string, Collection string, inputData map[string]interface{}) error {
+func saveData(DBname string, Collection string, inputData []map[string]interface{}) error {
+	Dir := "./Databases"
+	dbPath := filepath.Join(Dir, DBname)
+	CollectionPath := filepath.Join(dbPath, Collection+".json")
+
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
+		if err := os.MkdirAll(dbPath, os.ModePerm); err != nil {
+			fmt.Errorf("issue creating databases directory")
+		}
+	}
+
+	if _, err := os.Stat(CollectionPath); os.IsNotExist(err) {
+		if _, err := os.Create(CollectionPath); err != nil {
+			fmt.Errorf("issue creating databases collection")
+		}
+	}
+
+	jsonData, _ := ioutil.ReadFile(CollectionPath)
+	var existingData []map[string]interface{}
+	if len(jsonData) > 0 {
+		json.Unmarshal(jsonData, &existingData)
+	}
+	existingData = append(existingData, inputData...)
 
 	return nil
 }
